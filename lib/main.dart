51c384a8aa459224462d5f6d06c68b509aac441b
FABIS8844 <160301415+FABIS8844@users.noreply.github.com>
2024-03-20 17:04:51 +0530

    signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:loginapp/Signup.dart';
+import 'package:loginapp/welcome.dart';
 
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:Signup(),
+    home:Welcome(),
   ));
 }
